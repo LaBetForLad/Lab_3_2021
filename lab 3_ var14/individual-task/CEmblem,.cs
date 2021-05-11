84ef3567fe02eff1f8a36d9c383a8dd3af9b755e
Vladyslav <58324552+LaBetForLad@users.noreply.github.com>
2021-05-12 01:07:36 +0300

    Add files via upload

---
+++ lab 3_ var14/individual-task/CEmblem,.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace individual_task
+{
+    class CEmblem
+    {
+        public int x;
+        public int y;
+        public int size;
+        public int count;
+        public string s;
+        public bool re;
+
+
+       static Color col = Color.Black;
+        Pen pen = new Pen(col, 3);
+
+        Pen penB = new Pen(Color.Black, 3);
+        Pen penW = new Pen(Color.White, 3);
+
+
+        public CEmblem(int x, int y ,string s, int size)
+        {
+            this.x = x;
+            this.y = y;
+            this.s = s;
+            this.size = size;
+            re = false;
+        }
+
+
+        public void Draw(PictureBox b, Pen pen)
+        {
+            Graphics graphic = b.CreateGraphics();
+           
+            re = true;
+
+            graphic.DrawEllipse(pen, new Rectangle(x, y, this.size, this.size));
+
+            int xOfromb = (this.size / 2 + this.x);
+            int yOfromb = (this.size / 2 + this.y);
+
+            graphic.DrawPolygon(pen, new Point[4] { new Point(xOfromb - this.size / 2, yOfromb), new Point(xOfromb, yOfromb - this.size / 2), new Point(xOfromb + this.size / 2, yOfromb), new Point(xOfromb, yOfromb + this.size / 2) });         
+            graphic.DrawPolygon(pen, new PointF[4] { new PointF(xOfromb - (float)Math.Sqrt(2.0) * (this.size / 2), yOfromb), 
+                                                     new PointF(xOfromb, yOfromb - (this.size / 2) * (float)Math.Sqrt(2.0)), 
+                                                     new PointF(xOfromb + (this.size / 2) * (float)Math.Sqrt(2.0), yOfromb),
+                                                     new PointF(xOfromb, yOfromb + (this.size / 2) * (float)Math.Sqrt(2.0)) });
+        }
+
+        public void RotateEmbleb(PictureBox b, Pen pen)
+        {
+            Graphics graphic = b.CreateGraphics();
+          
+            re = false;
+          
+            int xOfromb = (this.size / 2 + this.x);
+            int yOfromb = (this.size / 2 + this.y);
+
+            graphic.DrawEllipse(pen, new Rectangle(x, y, this.size, this.size));
+            graphic.DrawRectangle(pen, new Rectangle(x, y, size, size));
+
+         //   graphic.DrawRectangle(pen, new Rectangle(xOfromb - 29, yOfromb - 29, Convert.ToInt32(size / Math.Sqrt(2)), Convert.ToInt32(size / Math.Sqrt(2))));
+            graphic.DrawRectangle(pen, new Rectangle(xOfromb - this.size / 3, yOfromb - this.size / 3, Convert.ToInt32(size / Math.Sqrt(2)), Convert.ToInt32(size / Math.Sqrt(2))));
+
+         //   int sidOfBigKvafrat = this.size * 2;
+         //   float sidOfSmall = this.size * 2 / (float)Math.Sqrt(2);
+
+          
+
+        }
+
+
+        public void Enlarge()
+        {
+            this.size++;
+        }
+        public void Reduce()
+        {
+            this.size--;
+        }
+
+
+        public void EndUp()
+        {
+            y = 0;
+        }
+        public void EndDown()
+        {
+            y = 435 - size;
+        }
+        public void EndLeft()
+        {
+            x = 0;
+        }
+        public void EndRight()
+        {
+            x = 478 - size;
+        }
+
+
+        public override string ToString()
+        {
+            return s ;
+        }
+
+
+
+        public void Hide()
+        {
+            col = Color.White;
+        }
+
+
+
+        public void MoveR()
+        {
+            if (x <= 478 - size)
+            {
+                x++;
+            }
+            else
+            {
+                MessageBox.Show("Нельзя выходить за границы холста");
+            }
+        }
+
+
+        public void MoveL()
+        {
+            if (x >= 1)
+            {
+                x--;
+            }
+            else
+            {
+                MessageBox.Show("Нельзя выходить за границы холста");
+            }
+        }
+
+
+
+        public void MoveUp()
+        {
+            if (x <= 435- this.size)
+            {
+                y++;
+            }
+            else
+            {
+                MessageBox.Show("Нельзя выходить за границы холста");
+            }
+        }
+
+        public void MoveDown()
+        {
+            if (y >= 1)
+            {
+                y--;
+            }
+            else
+            {
+                MessageBox.Show("Нельзя выходить за границы холста");
+            }
+        }
+
+
+
+        public void Show()
+        {
+            col = Color.Black;
+        }
+    }
+}
---
+++ lab 3_ var14/individual-task/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace individual_task
+{
+    public partial class Form1 : Form
+    {
+        CEmblem b;
+        int count = 0;
+
+        Pen penB = new Pen(Color.Black, 3);
+        Pen penW = new Pen(Color.White, 3);
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+              
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.MoveR();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.MoveR();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.Enlarge();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.Enlarge();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+
+           
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CEmblem ob = new CEmblem(new Random().Next(0, 500), new Random().Next(0, 435), "эмблема" + " " + count, 80);
+            comboBox1.Items.Add(ob);
+            count++;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+                
+                emblem.Draw(pic,penB);
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+
+
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                   
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                   
+                }
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+
+              
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.Reduce();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.Reduce();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.EndUp();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.EndUp();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+
+                emblem.Draw(pic, penW);
+                emblem.Draw(pic, penB);
+                emblem.EndDown();
+                emblem.Show();
+                emblem.Draw(pic, penB);
+
+
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.EndDown();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.EndDown();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+
+                if(emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.MoveL();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.MoveL();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+               
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+          
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.MoveDown();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.MoveDown();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+
+          
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.MoveUp();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.MoveUp();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+          
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.EndRight();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.EndRight();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+           
+                if (emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.EndLeft();
+                    emblem.Draw(pic, penB);
+                }
+                else
+                {
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.EndLeft();
+                    emblem.RotateEmbleb(pic, penB);
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                CEmblem emblem = (CEmblem)comboBox1.SelectedItem;
+
+                if(emblem.re)
+                {
+                    emblem.Draw(pic, penW);
+                    emblem.RotateEmbleb(pic,penB);
+                }
+                else
+                {
+                  
+                    emblem.RotateEmbleb(pic, penW);
+                    emblem.Draw(pic, penB);
+                }
+
+                
+              
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
